d928c7dfced13831f697052ebf3119f066a13ab8
ahamax955 <2513672259@qq.com>
2025-05-13 23:35:36 +0800

    添加readme文件

---
+++ main.py
@@ -0,0 +1,25 @@
+import pymysql
+
+
+def get_flag():
+    conn = pymysql.Connect(
+        host='47.93.91.229',
+        port=3306,
+        user='temu',
+        passwd='aexyLEnRh83WFdA2',
+        db='temu',
+        charset='utf8')
+    cursor = conn.cursor()
+    sql = "select * from userinfo"
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    for row in results:
+        print(row)
+    cursor.close()
+    conn.close()
+
+
+
+
+if __name__ == '__main__':
+    print(get_flag())
